1474b1786056afaeca3ba3afb1d891b5a5a77c1b
zhangpeng.1024 <zhangpeng.1024@bytedance.com>
2022-02-22 18:40:14 +0800

    Home Page

---
+++ page/src/components/HomePage.vue
@@ -0,0 +1,74 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        原神伤害期望计算器
+      </h1>
+      <div class="notification is-success">
+        请输入<strong>暴击率</strong>、<strong>暴击伤害加成</strong>、<strong>暴击伤害数值</strong>
+      </div>
+
+      <p class="subtitle">
+
+      </p>
+      <div class="field">
+        <label class="label">暴击率</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="百分号前面的数字，例如70">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">暴击伤害加成</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="百分号前面的数字，例如150">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">暴击伤害数值</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="实际暴击伤害的数值">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">伤害期望</label>
+        <div class="control">
+          <input class="input" readonly type="text" placeholder="计算后显示">
+        </div>
+      </div>
+
+      <button id="submit" class="button is-primary">计算</button>
+
+
+
+    </div>
+  </section>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomePage',
+  props: {
+    // msg: String
+  },
+  data() {
+    return {
+      criticalChance: 0,
+      criticalAddition: 0,
+      criticalDamage: 0
+    }
+  },
+  created() {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css";
+</style>
